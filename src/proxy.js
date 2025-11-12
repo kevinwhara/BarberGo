d207e0a8aeeb04017007b6529c308e16611c72ca
Kevin Denniswhara <wibowokevin425@gmail.com>
2025-11-12 08:20:35 +0700

    [Migration] Middleware to Proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export function middleware(request) {
+export function proxy(request) {
   const url = request.nextUrl.clone()
   const user = request.cookies.get('user')?.value
   const role = request.cookies.get('role')?.value
